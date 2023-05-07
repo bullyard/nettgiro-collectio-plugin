97418cbc92d5181b0f2de152202c6b305f240f30
Rodrigo Perez <public_email@bullyard.no>
2023-05-07 03:31:24 +0200

    Moved invoice option checkbox to a hook

--- functions.php
+++ functions.php
@@ -13,6 +13,60 @@ use Bullyard\Predator\PredatorCaseImport;
 
 /* HOOKS */
 
+$hooks->add_action('invoice_option_checkbox', 'collectio_invoice_option_checkbox_fn');
+function collectio_invoice_option_checkbox_fn($uid, $invoiceType){
+    
+    if ($invoiceType == 1){
+        // check if user has accepted terms
+        $acceptedTerms = Metadata::get('collectio_terms', $uid);
+
+        // checked if user has accepted terms
+        $isChecked = ($acceptedTerms) ? "checked='checked'" : "";
+
+        // uncheck if admin
+        if (isset($_SESSION['admin']) && $_SESSION['admin'] == true){
+            $isChecked = "";
+        }
+
+        ?>
+        <div class="col-form-label col-12 col-sm-8  pl-4 offset-sm-4 custom-control custom-checkbox">
+            <input type="checkbox" name="collectionService" id="collectionService" class="custom-control-input"
+                value='1' <?=($acceptedTerms?"":"disabled='disabled'")?> <?=$isChecked?> />
+            <label class="custom-control-label" for="collectionService">
+                Automatisk purring/inkasso ved manglende betaling.
+                <?php 
+                    if ($acceptedTerms == false){
+                        echo '<a href="#" class="badge badge-warning rounded-circle badge-warning-alt ml-2 " data-toggle="collapse" data-target="#infoPluginAcceptTerms" aria-expanded="false" aria-controls="infoPluginAcceptTerms">?</a>';
+                    }
+                ?>
+            </label>
+
+            <div class="form-group form-row mb-0" id="collectionServiceInfo">
+                <?php 
+                    if ($acceptedTerms !== false){
+                        if (CONF_priceCollectorFee > 0){ ?>
+                        <div class="col-12 mt-2">
+                            <?php
+                                (new AlertHelper('Automatisk purring og inkasso', 'Pris: NOK <strong>'.CONF_priceCollectorFee.'.-</strong> <small>eks. MVA</small> for tjenesten når saken blir overført til partner.', false))->icon('!')->id('collectio_show_price')->type('red')->alert(true);
+                            ?>
+                        </div>
+                    <?php 
+                        } 
+                    }else{ 
+                        echo "<div id='infoPluginAcceptTerms' class='collapse mt-2'>";
+                            AlertHelper::al('Aktiver purring og Inkasso', 'For å kunne aktivere oppfølging av dine saker må du først <a href="#" class="collectio_accept_terms"> godta vilkår og betingelser</a>.', 'warning', '?', false);
+                        echo "</div>";
+                    }
+                ?>
+            </div>
+        </div>
+
+        <?php 
+    }
+
+    
+};
+
 $hooks->add_action('terms_page', 'collectio_terms_fn');
 function collectio_terms_fn(){
     echo "<h5 class='pt-2 font-weight-normal'>Brukerbetingelser for Automatisk purring og inkasso via Collectio AS</h5>";
